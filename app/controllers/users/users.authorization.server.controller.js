54aed1547f1c1c41bd51bde67553af22b83aaee2
nach0 <nachogarcia91@gmail.com>
2015-05-27 17:17:36 +0200

    Removed unnecessary authentication II

--- app/controllers/users/users.authorization.server.controller.js
+++ app/controllers/users/users.authorization.server.controller.js
@@ -25,11 +25,13 @@ exports.userByID = function(req, res, next, id) {
  * Require login routing middleware
  */
 exports.requiresLogin = function(req, res, next) {
+	/**
 	if (!req.isAuthenticated()) {
 		return res.status(401).send({
 			message: 'User is not logged in'
 		});
 	}
+	*/
 
 	next();
 };
